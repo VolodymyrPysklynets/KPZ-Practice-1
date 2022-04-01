68f996133a391b0318ff6f77bc7f86942ffc5b89
Volodymyr <99961489+VolodymyrPysklynets@users.noreply.github.com>
2022-04-01 12:29:47 +0300

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,9 @@
+def prime_numbers_generator(n):
+    prime_numbers = []
+    for number in range(2, n + 1):
+        for prime in prime_numbers:
+            if not number % prime:
+                break
+        else:
+            prime_numbers.append(number)
+            yield number
